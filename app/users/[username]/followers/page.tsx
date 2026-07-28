509524840b3bc5bab1f5f4ff3e44c7f0f8671728
MasterCodeTeams <mastercodeapps.id@gmail.com>
2026-07-28 14:05:46 +0800

    Delete app

--- app/users/[username]/followers/page.tsx
+++
@@ -1,5 +0,0 @@
-import UserListPage from "@/components/UserListPage";
-
-export default function FollowersPage({ params }: { params: { username: string } }) {
-  return <UserListPage username={params.username} kind="followers" />;
-}
